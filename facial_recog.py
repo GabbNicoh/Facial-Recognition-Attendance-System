bf5c32f39c4bf6574f5802a74249e15836898732
Gabb <40926666+GabbNicoh@users.noreply.github.com>
2023-01-03 11:53:16 +0800

    annotated file

--- facial_recog.py
+++ facial_recog.py
@@ -4,9 +4,9 @@ import face_recognition
 import os
 from datetime import datetime
 
-path = 'image'
 images = []
 classNames = []
+path = 'image'
 myList = os.listdir(path)
 
 for cl in myList:
@@ -18,7 +18,7 @@ print(classNames)
 def findEncodings(images):
     encodeList = []
     for img in images:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # change to gray
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
@@ -28,19 +28,26 @@ print('Encoding Complete')
 
 cap = cv2.VideoCapture(0)
 
+if not cap.isOpened():
+    print('ERROR! No video source found...')
+
 while True:
     success, img = cap.read()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB) # change to gray
 
+    # find all the faces and face encodings in current frame of video
     facesCurFrame = face_recognition.face_locations(imgS)
     encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        matchIndex = np.argmin(faceDis)
 
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        # see if face is a match for known faces
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        # Calculate shortest distance from face
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+
+        matchIndex = np.argmin(faceDis)
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
 
