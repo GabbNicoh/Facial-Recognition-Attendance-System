a839383ac6391827664ccf9ca6b55cdae5c49597
Gabb <40926666+GabbNicoh@users.noreply.github.com>
2023-01-05 12:47:01 +0800

    added logging algorithm, to implement to face

--- attendance_logging.py
+++ attendance_logging.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+import threading
+
+# TODO: INCREMENT FACE TO REACH A PONIT IF POINT REACHED THEN LOG
 
 def Attendance(name):
     with open('attendance.csv', 'r+') as f:
@@ -14,17 +17,41 @@ def Attendance(name):
             dtString = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
 
-Attendance('Test')
+# Attendance('Test')
 
+logStatus = {}
 def Logging(name):
     with open('logging.csv', 'r+') as f:
         myDataList = f.readlines()
         nameList = []
+        status = ''
         for line in myDataList:
             entry = line.split(',')
             nameList.append(entry[0])
+            
         if name not in nameList:
             now = datetime.now()
-            dtString = now.strftime('%H:%M:%S')
-            f.writelines(f'\n{name},{dtString}')
-        isIn = False
+            time = now.strftime('%H:%M:%S')
+            status = 'IN'
+            logStatus[name] = status
+            f.writelines(f'\n{name},{time},{status}')
+        
+        if name in nameList:
+            if logStatus[name] == 'IN': 
+                now = datetime.now()
+                time = now.strftime('%H:%M:%S')
+                status = 'OUT'
+                logStatus[name] = status
+                nameList.remove(name)
+                f.writelines(f'\n{name},{time},{status}')
+
+        print(f'DICTIONARY: {logStatus}')
+        print(f'Namelist: {nameList}')
+            
+Logging('YourMom')
+
+event = threading.Event()
+event.wait(3)
+
+Logging('JackDaniaels')
+Logging('YourMom')
