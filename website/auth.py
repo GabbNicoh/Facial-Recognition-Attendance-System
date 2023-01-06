3fe1e09c751b725b137f5de055e26310bdd8fb65
Gabb <40926666+GabbNicoh@users.noreply.github.com>
2023-01-06 14:54:33 +0800

    added login page

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,9 @@
 # logins
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
-
-@auth.route('/logout')
-def logout():
-    return "<p>Logout</p>"
+    return render_template('login.html')
