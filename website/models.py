f373206b439699ae737065321cbfff682f7ff058
Gabb <40926666+GabbNicoh@users.noreply.github.com>
2023-01-08 11:23:41 +0800

    model relationships

--- website/models.py
+++ website/models.py
@@ -12,21 +12,25 @@ class Attendance_Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     atdc_name = db.Column(db.Integer, unique=True)
     atdc_date = db.Column(db.DateTime(timezone=True), default=func.now)
+    attdc_record = db.relationship('Attendance_Record')
 
 class Log_Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     log_name = db.Column(db.String(150))
     log_time = db.Column(db.DateTime(timezone=True), default=func.now)
     log_status = db.Column(db.Stringg(150))
+    attdc_record = db.relationship('Log_Record')
 
 class Attendance_Record(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     atdc_id = db.Column(db.Integer, db.ForeignKey('Attendance_Subject.id'))
+    subjects = db.relationship('Subjects')
     # subject_id = db.Column(db.String(40), db.ForeignKey())
 
 class Log_Record(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     log_id = db.Column(db.Integer, db.ForeignKey('Attendance_Subject.id'))
+    subjects = db.relationship('Subjects')
     
 
 class Subjects(db.Model):
