fae2343d700a4a394b3a74b5979418970b5588b3
Gabb <40926666+GabbNicoh@users.noreply.github.com>
2023-01-08 11:07:52 +0800

    added model

--- website/models.py
+++ website/models.py
@@ -8,13 +8,28 @@ class User(db.Model, UserMixin):
     id_num = db.Column(db.Integer, unique=True)
     password = db.Column(db.String(150))
 
-class Attendance_Record(db.Model):
+class Attendance_Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     atdc_name = db.Column(db.Integer, unique=True)
     atdc_date = db.Column(db.DateTime(timezone=True), default=func.now)
 
-class Log_Record(db.Model):
+class Log_Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     log_name = db.Column(db.String(150))
     log_time = db.Column(db.DateTime(timezone=True), default=func.now)
     log_status = db.Column(db.Stringg(150))
+
+class Attendance_Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    atdc_id = db.Column(db.Integer, db.ForeignKey('Attendance_Subject.id'))
+    # subject_id = db.Column(db.String(40), db.ForeignKey())
+
+class Log_Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    log_id = db.Column(db.Integer, db.ForeignKey('Attendance_Subject.id'))
+    
+
+class Subjects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    atdc_id = db.Column()
+    log_id = db.Column()
