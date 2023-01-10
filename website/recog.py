0342723c30eb31d0ae00478e96a1dd6959c8d409
Gabb <40926666+GabbNicoh@users.noreply.github.com>
2023-01-10 15:56:55 +0800

    without database

--- website/recog.py
+++ website/recog.py
@@ -9,7 +9,6 @@ import mysql.connector
 from flask_login import login_user, login_required, logout_user, current_user
 import pandas as pd
 
-# app = Flask(__name__)
 recog = Blueprint('recog', __name__)
 
 stdNames = []
@@ -17,8 +16,6 @@ encodeStdKnown = []
 path = 'image'
 myList = os.listdir(path)
 
-# df = pd.read_csv("website/attendance.csv")
-# df.to_csv("website/attendance.csv", index=None)
 for cl in myList:
     stdNames.append(os.path.splitext(cl)[0])
 print(stdNames)
@@ -124,7 +121,7 @@ def main_face_recog():
                 if timer == 6: # if lasts for 6 seconds log
                     Logging(name)
                     Attendance(name)
-                    csv_database_attendance()
+                    # csv_database_attendance()
                     found = True
                 
                 detected_faces.append(f'{name}')
@@ -161,116 +158,104 @@ def main_face_recog():
         img = buffer.tobytes()
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')
 
-# @recog.route('/face-recog', methods = ["POST", "GET"])
-# @login_required
-# def index():
-#     # ADD HERE FUNCTION FOR GRABBING FROM ATTENDANCE AND SAVING FROM CSV TO DATABASE (base from auth)
-#     if request.method == "POST":
-#         mydb = mysql.connector.connect(host='localhost', user='root', password='0170', database='facedb')
-#         cur = mydb.cursor()
-#         cur.execute("SELECT * FROM AttendanceSubject")
-#         output = cur.fetchall()
-#         cur.close()
-#         return render_template("face-recog.html", data=output)
-#     else:
-#         return render_template("face-recog.html")
-
 
 @recog.route('/face-recog', methods = ["POST", "GET"])
 def index():
     # ADD HERE FUNCTION FOR GRABBING FROM ATTENDANCE AND SAVING FROM CSV TO DATABASE (base from auth)
-    if request.method == "POST":
-        mydb = mysql.connector.connect(host='localhost', user='root', password='0170', database='facedb')
-        cur = mydb.cursor()
-        cur.execute("SELECT * FROM AttendanceSubject")
-        attd_output = cur.fetchall()
-        cur.close()
+    # if request.method == "POST":
+    #     mydb = mysql.connector.connect(host='localhost', user='root', password='0170', database='facedb')
+    #     cur = mydb.cursor()
+    #     cur.execute("SELECT * FROM AttendanceSubject")
+    #     attd_output = cur.fetchall()
+    #     cur.close()
 
-        at_name = []
-        at_time = []
+    #     at_name = []
+    #     at_time = []
 
-        for line in attd_output:
-            newLine = list(line)
-            at_name.append(newLine[1])
-            at_time.append(newLine[2])
+    #     for line in attd_output:
+    #         newLine = list(line)
+    #         at_name.append(newLine[1])
+    #         at_time.append(newLine[2])
 
-        return render_template("face-recog.html", att_name = at_name, att_time = at_time, at_len = len(at_name))
-    else:
-        return render_template("face-recog.html", att_name = "", att_time = "", at_len=0)
+    #     return render_template("face-recog.html", att_name = at_name, att_time = at_time, at_len = len(at_name))
+    # else:
+    #     return render_template("face-recog.html", att_name = "", att_time = "", at_len=0)
+        return render_template("face-recog.html")
         
 
 @recog.route('/video_feed')
 def video_feed():
     return Response(main_face_recog(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def csv_database_attendance():
-    mydb = mysql.connector.connect(host='localhost', user='root', password='0170', database='facedb')
-    print('database connected')
-    cursor = mydb.cursor()
-    csv_data = csv.reader(open('website/attendance.csv'))
-    # for attendance
-    next(csv_data)
-    for row in csv_data:
-        try:
-            cursor.execute('INSERT INTO attendancesubject (id,atdc_name,atdc_date) VALUES(%s,%s,%s)', row)
-            print(row)
-        except mysql.connector.errors.IntegrityError:
-            continue
+# def csv_database_attendance():
+#     mydb = mysql.connector.connect(host='localhost', user='root', password='0170', database='facedb')
+#     print('database connected')
+#     cursor = mydb.cursor()
+#     csv_data = csv.reader(open('website/attendance.csv'))
+#     # for attendance
+#     next(csv_data)
+#     for row in csv_data:
+#         try:
+#             cursor.execute('INSERT INTO attendancesubject (id,atdc_name,atdc_date) VALUES(%s,%s,%s)', row)
+#             print(row)
+#         except mysql.connector.errors.IntegrityError:
+#             continue
 
-    mydb.commit()
-    cursor.close()
+#     mydb.commit()
+#     cursor.close()
 
 @recog.route('/view', methods = ["POST", "GET"])
-@login_required
+# @login_required
 def csv_database_log():
-    mydb = mysql.connector.connect(host='localhost', user='root', password='0170', database='facedb')
-    print('database connected')
-    cursor = mydb.cursor()
-    csv_data = csv.reader(open('website/logging.csv'))
-    # for log
-    next(csv_data)
-    for row in csv_data:
-        try:
-            cursor.execute('INSERT INTO logsubject (id,log_name,log_time,log_status) VALUES(%s,%s,%s,%s)', row)
-            print(row)
-        except mysql.connector.errors.IntegrityError:
-            continue
+    # mydb = mysql.connector.connect(host='localhost', user='root', password='0170', database='facedb')
+    # print('database connected')
+    # cursor = mydb.cursor()
+    # csv_data = csv.reader(open('website/logging.csv'))
+    # # for log
+    # next(csv_data)
+    # for row in csv_data:
+    #     try:
+    #         cursor.execute('INSERT INTO logsubject (id,log_name,log_time,log_status) VALUES(%s,%s,%s,%s)', row)
+    #         print(row)
+    #     except mysql.connector.errors.IntegrityError:
+    #         continue
 
-    mydb.commit()
-    cursor.close()
+    # mydb.commit()
+    # cursor.close()
 
-    # clear csv here?
-    if request.method == "POST":
-        mydb = mysql.connector.connect(host='localhost', user='root', password='0170', database='facedb')
-        cur = mydb.cursor()
-        cur.execute("SELECT * FROM AttendanceSubject")
-        attd_output = cur.fetchall()
-        cur.execute("SELECT * FROM LogSubject")
-        lg_output = cur.fetchall()
-        cur.close()
+    # # clear csv here?
+    # if request.method == "POST":
+    #     mydb = mysql.connector.connect(host='localhost', user='root', password='0170', database='facedb')
+    #     cur = mydb.cursor()
+    #     cur.execute("SELECT * FROM AttendanceSubject")
+    #     attd_output = cur.fetchall()
+    #     cur.execute("SELECT * FROM LogSubject")
+    #     lg_output = cur.fetchall()
+    #     cur.close()
 
-        at_name = []
-        at_time = []
+    #     at_name = []
+    #     at_time = []
 
-        for line in attd_output:
-            newLine = list(line)
-            at_name.append(newLine[1])
-            at_time.append(newLine[2])
+    #     for line in attd_output:
+    #         newLine = list(line)
+    #         at_name.append(newLine[1])
+    #         at_time.append(newLine[2])
 
-        lg_name = []
-        lg_time = []
-        lg_status = []
+    #     lg_name = []
+    #     lg_time = []
+    #     lg_status = []
 
-        for line in lg_output:
-            newLine = list(line)
-            lg_name.append(newLine[1])
-            lg_time.append(newLine[2])
-            lg_status.append(newLine[3])
+    #     for line in lg_output:
+    #         newLine = list(line)
+    #         lg_name.append(newLine[1])
+    #         lg_time.append(newLine[2])
+    #         lg_status.append(newLine[3])
 
-        return render_template("view.html", att_name=at_name, att_time=at_time, lgg_name=lg_name, lgg_time=lg_time,
-                               lgg_status=lg_status, at_len=len(at_name), lg_len=len(lg_name))
-    else:
-        return render_template("view.html", at_len=0, lg_len=0, att_name="", att_time="", lgg_name="", lgg_time="", lgg_status="")
+    #     return render_template("view.html", att_name=at_name, att_time=at_time, lgg_name=lg_name, lgg_time=lg_time,
+    #                            lgg_status=lg_status, at_len=len(at_name), lg_len=len(lg_name))
+    # else:
+    #     return render_template("view.html", at_len=0, lg_len=0, att_name="", att_time="", lgg_name="", lgg_time="", lgg_status="")
+    return render_template("view.html")
 
 from glob import glob
 from io import BytesIO
--- website/views.py
+++ website/views.py
@@ -9,27 +9,28 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/')
-@login_required
+# @login_required
 def home():
-    mydb = mysql.connector.connect(host='localhost', user='root', password='0170', database='facedb')
-    cur = mydb.cursor()
-    cur.execute(
-        "SELECT Student_ID, Last_Name, First_Name, Student_Status FROM class_list where Class_Subject_ID = 'CSC 0312.1' ORDER BY Last_Name;")
-    output = cur.fetchall()
-    cur.close()
+    # mydb = mysql.connector.connect(host='localhost', user='root', password='0170', database='facedb')
+    # cur = mydb.cursor()
+    # cur.execute(
+    #     "SELECT Student_ID, Last_Name, First_Name, Student_Status FROM class_list where Class_Subject_ID = 'CSC 0312.1' ORDER BY Last_Name;")
+    # output = cur.fetchall()
+    # cur.close()
 
-    StdID = []
-    Lst_Name = []
-    Frst_Name = []
-    Std_Status = []
+    # StdID = []
+    # Lst_Name = []
+    # Frst_Name = []
+    # Std_Status = []
 
-    for line in output:
-        print(line)
-        field = list(line)
-        StdID.append(field[0])
-        Lst_Name.append(field[1])
-        Frst_Name.append(field[2])
-        Std_Status.append(field[3])
+    # for line in output:
+    #     print(line)
+    #     field = list(line)
+    #     StdID.append(field[0])
+    #     Lst_Name.append(field[1])
+    #     Frst_Name.append(field[2])
+    #     Std_Status.append(field[3])
 
-    return render_template("home.html", len=len(StdID), stdID=StdID, lstName=Lst_Name, frstName=Frst_Name,
-                           StdStatus=Std_Status, user=current_user)
+    # return render_template("home.html", len=len(StdID), stdID=StdID, lstName=Lst_Name, frstName=Frst_Name,
+    #                        StdStatus=Std_Status, user=current_user)
+    return render_template("home.html")
